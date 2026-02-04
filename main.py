daa8f0b8799bb6a4da2e460b236fa486b6a9f5c5
Xc_Star <2035374188@qq.com>
2026-02-04 19:46:25 +0800

    fix: 安装报错

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import random
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -19,6 +20,15 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def main(self, event: AstrMessageEvent):
+        if random.random() < 0.5:
+            if event.get_sender_id() == '2187211506':
+                    yield event.plain_result(f"抓到小钊了！撅！")
+            if event.get_sender_id() == '2601539858':
+                    yield event.plain_result(f"抓到晚枫了！撅！")
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
